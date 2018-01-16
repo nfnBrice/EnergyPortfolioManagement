9774f21ceca0929e938757e641e1ee5791cf90a3
Alice nagel <Alice@MacBook-Pro-de-Alice.local>
2018-01-16 14:36:36 +0100

    comments

--- LargeApp/app/hello.py
+++ LargeApp/app/hello.py
@@ -1,1 +1,19 @@
-from
+#IMPORT FLASK
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+#WEBPAGES INITIALISATION 
+#home webpage init
+@app.route('/')
+def home():
+    return render_template('home.html')
+#about webpage init 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+#debug mode -> put to false when dev mode is finished
+if __name__ == '__main__':
+    app.run(debug=True)
