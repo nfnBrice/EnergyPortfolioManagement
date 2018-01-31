a7987be0e8d08ce4d501c0aa1959bfb800507b11
Alice nagel <Alice@MacBook-Pro-de-Alice.local>
2018-01-31 17:03:29 +0100

    new master

--- LargeApp/app/hello.py
+++ LargeApp/app/hello.py
@@ -1,13 +1,15 @@
 #IMPORT FLASK
 from flask import Flask, render_template, json, request
-from flaskext.mysql import MySQL
+from flask.ext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 from flask import session
 from flask import redirect
 import string
+import requests
+import optimiz as optimiz
+import pandas as pd
 app = Flask(__name__)
 app.secret_key = 'why would I tell you my secret key?'
-
 
 #data base configuration 
 mysql = MySQL()
@@ -34,6 +36,50 @@ def showSignUp():
 @app.route('/showSignIn')
 def showSignin():
 	return render_template('signin.html')
+	
+@app.route("/projects")
+def project():
+    return render_template('projects.html')	
+
+@app.route("/showCreatePortfolio")
+def showCreatePortfolio():
+    return render_template('createPortfolio.html')
+
+#page de tutoriel à faire avant cette page la 
+@app.route('/createPortfolio', methods=['POST','GET'])
+def createPortfolio():
+	#create mysql connection
+	conn = mysql.connect()
+	#create cursor
+	cursor = conn.cursor()
+	try: 
+		# read the posted values from the UI
+		_amount = request.form['inputAmount']
+		_horizon = request.form['inputHorizon']
+		_knowledge = request.form['inputKnowledge']
+
+		# validate the received values
+		if _amount and _horizon and _knowledge:
+			if _knowledge is 0:
+				return render_template('tutorial.html')
+			else :
+				#find portfolio 
+				cursor.callproc('sp_createPortfolio', (_amount, _horizon, session['user']))
+				data = cursor.fetchall()
+				if len(data) is 0:
+					return json.dumps({'error':str(data[0])})
+				else:
+					conn.commit()
+					session['portfolio'] = data[0][0] 
+					return redirect('/showAddStocks')
+
+		else:
+			return json.dumps({'html':'<span>Enter the required fields</span>'})
+	#except Exception as e:
+	#	return json.dumps({'error':str(e)})
+	finally:
+		cursor.close() 
+		conn.close()
 
 @app.route('/userHome')
 def userHome():
@@ -64,7 +110,7 @@ def signUp():
 			#password generation
 			_hashed_password = generate_password_hash(_password)
 			#call the procedure create user 
-			cursor.callproc('sp_createUser',(_name,_email,_password))
+			cursor.callproc('sp_createUser',(_email,_name,_hashed_password))
 			#test to know if the data was well created 
 			data = cursor.fetchall()
 			if len(data) is 0:
@@ -82,28 +128,116 @@ def signUp():
 
 #SignIn procedure
 @app.route('/validateLogin',methods=['POST'])
-def validateLogin():
+def signIn():
 	try:
 		_email = request.form['inputEmail']
 		_password = request.form['inputPassword']
 		
-		con = mysql.connect()
-		cursor = con.cursor()
-		cursor.callproc('sp_connect', _email) #for some weird reason email is not one element but the number of char it is composed of 
+		conn = mysql.connect()
+		cursor = conn.cursor()
+		cursor.callproc('sp_connect', (_email,)) #for some weird reason email is not one element but the number of char it is composed of 
 		data = cursor.fetchall()
-
+		
 		if len(data) > 0:
-
+			session['user'] = data[0][0]
 			if check_password_hash(str(data[0][3]),_password):
 				session['user'] = data[0][0]
-				return redirect('/userHome')
+				return redirect('/showCreatePortfolio')
 			else:
 				return render_template('error.html',error = 'Wrong Email address or Password.')
 		else:
-			return render_template('error.html',error = 'caca')
+			#return render_template('error.html',error = 'caca')
+			return render_template('index.html')
 
 	except Exception as e:
 		return render_template('error.html',error = str(e))	
+
+	finally:
+		cursor.close() 
+		conn.close()	
+
+@app.route('/showAddStocks')
+def showStocks():
+	try:
+		#create mysql connection
+		conn = mysql.connect()
+		#create cursor
+		cursor = conn.cursor()
+		cursor.callproc('sp_getAllStocks', ())
+		data = cursor.fetchall()
+		return render_template('addStocks.html', data=data)
+	except Exception as e:
+		return render_template('error.html',error = str(e))	
+		
+	finally:
+		cursor.close() 
+		conn.close()
+
+@app.route('/showPortfolio')
+def showPortfolio():
+	try: 
+		#create mysql connection
+		conn = mysql.connect()
+		#create cursor
+		cursor = conn.cursor()
+
+		amountList=[ ]
+		stocksOfCurrentPortfolio = [ ]
+		cursor.callproc('sp_getLinkDataFromPortfolioID',(session['portfolio'],))
+		data = cursor.fetchall()
+		i=0
+		for each in data:
+			amountList.append(each[2])
+			cursor.callproc('sp_getStockInfoFromLinkID',(each[4],))
+			CurrentStocks = cursor.fetchall()
+			#CurrentStocksdf = pd.DataFrame(CurrentStocks[0])
+			stocksOfCurrentPortfolio.append(CurrentStocks[0])
+			#stocksOfCurrentPortfolio[0].remove(stocksOfCurrentPortfolio[i][0])
+			i=i+1
+		df = pd.DataFrame(stocksOfCurrentPortfolio)
+		df['amount'] = amountList
+		df.rename(
+            columns={0: 'id', 1: 'code', 2: 'name', 'amount': 'weight'},
+            inplace=True
+        )
+		del df['id']
+		return render_template('portfolio.html', data=df.to_html())
+	#except Exception as e:
+	# 	return render_template('error.html',error = str(e))	
+	finally:
+		cursor.close() 
+		conn.close()
+
+#add Bonds to the portfolio
+@app.route('/addStocks',methods=['POST'])
+def addStocks():
+	conn = mysql.connect()
+	#create cursor
+	cursor = conn.cursor()
+	try:
+		cursor.callproc('sp_getLinkDataFromPortfolioID', (session['portfolio'],))
+		isThereLinksInPortfolio = cursor.fetchall()
+
+		#if isThereLinksInPortfolio means the portfolio is full -> ask user to modify it or create a new one. 
+		if(not isThereLinksInPortfolio):
+			f = request.form
+			caca=[ ]
+
+			for key in f.keys():
+				session['key']=int(key);
+				cursor.callproc('sp_linkStockToPortfolio', (session['portfolio'], session['key']))
+				portfolioLinkAdded = cursor.fetchall()
+				caca.append(portfolioLinkAdded[0][4])
+				conn.commit()
+
+			#print(caca)
+			optimiz.optimiz(caca)
+		return redirect('/showPortfolio')
+		
+	finally:
+		cursor.close() 
+		conn.close()
+
 
 #debug mode -> put to false when dev mode is finished
 if __name__ == '__main__':
