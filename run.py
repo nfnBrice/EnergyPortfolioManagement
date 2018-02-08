fc578e7c0ad98d8eca5156ef2e29060c6a769f4d
thefeine <mangathefeine@gmail.com>
2018-02-08 15:00:35 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, redirect
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+@app.route("/projects")
+def project():
+    return render_template('projects.html')
+
+if __name__ == "__main__":
+    app.run()
